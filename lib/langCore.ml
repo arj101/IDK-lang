ddd48ddd004d04b37267079a1dcad66b3bf51892
arduino explorer <arjunanmr101@gmail.com>
2023-07-23 15:38:50 +0530

    add array.flatten

--- lib/langCore.ml
+++ lib/langCore.ml
@@ -118,7 +118,7 @@ let array_slice _ = function
           (int_of_float (Float.round (end_exc -. start)))
       in
       Array (ref new_elements)
-  | [Array elements; Literal (Num start)] ->
+  | [ Array elements; Literal (Num start) ] ->
       let end_exc = float_of_int (Array.length !elements) in
       let start = if start < 0. then end_exc +. start else start in
       let new_elements =
@@ -127,6 +127,37 @@ let array_slice _ = function
           (int_of_float (Float.round (end_exc -. start)))
       in
       Array (ref new_elements)
+  | _ -> raise TypeError
+
+let array_flatten _ args =
+  let rec flatten_aux depth a =
+    if depth < 0 then a
+    else
+      let rec flatten_aux_aux a acc subi_start subi_curr =
+        if Array.length a > subi_curr then
+          match Array.get a subi_curr with
+          | Array suba ->
+              flatten_aux_aux a
+                (Array.concat
+                   [
+                     acc;
+                     Array.sub a subi_start
+                       (Int.max (subi_curr - subi_start - 1) 0);
+                     flatten_aux (depth - 1) !suba;
+                   ])
+                (subi_curr + 1) (subi_curr + 1)
+          | elem -> flatten_aux_aux a acc subi_start (subi_curr + 1)
+        else
+          Array.concat
+            [ acc; Array.sub a subi_start (Array.length a - subi_start) ]
+      in
+      flatten_aux_aux a [||] 0 0
+  in
+  match args with
+  | Array elements :: Literal (Num depth) :: _ ->
+      Array
+        (ref (flatten_aux (int_of_float (Float.round depth -. 1.)) !elements))
+  | Array elements :: _ -> Array (ref (flatten_aux 0 !elements))
   | _ -> raise TypeError
 
 let gen_array_obj parent_env : value =
@@ -145,5 +176,6 @@ let gen_array_obj parent_env : value =
   def_fn "foldl" [ "array"; "fn_acc_v"; "init" ] array_foldl;
   def_fn "foldr" [ "array"; "fn_v_acc"; "init" ] array_foldr;
   def_fn "slice" [ "array"; "start"; "end_exclusive" ] array_slice;
+  def_fn "flatten" [ "array" ] array_flatten;
 
   array_obj
